56976fdabf918b9955e3d6ec4f3b8349ffd538f5
Ji-Yuhang <yuhang.silence@gmail.com>
2017-04-02 01:52:53 +0800

    修改新的模板和样式

--- js/content.js
+++ js/content.js
@@ -1,3 +1,5 @@
+var mousePos;
+var last_shanbay_data;
 function search_selected_text() {
     let text = window.getSelection().toString().trim().match(/^[a-zA-Z\s']+$/);
     let anchor_node_text = window.getSelection().anchorNode.wholeText
@@ -9,25 +11,56 @@ function search_selected_text() {
         anchor_node_text: anchor_node_text
     });
 }
+function playAudio(audio_url) {
+    chrome.runtime.sendMessage({method: "playAudio", data: {audio_url: audio_url}})
+}
 $(function () {
     $(document).on('dblclick', search_selected_text);
     let popover_html = '<div id="popover_html" class="popover_html" ></div>';
     // style="display: none;"
     $('body').append(popover_html);
-    $("#popover_html").on('click', function () {
-        $("#popover_html").css("display", "none");
+    $("#popover_html").on('click', function (e) {
+        e.preventDefault();
+        return false;
+        // $("#popover_html").css("display", "none");
     });
+
 });
 function shanbay_template(data) {
     return (
         `
         <div>
-            ${data.definition}
+            <div class="shanbay_head">
+                <div>
+                    <span id="shanbay_content">
+                        ${data.content}</span>
+                    <span id="shanbay_pronunciation">
+                        [${data.pronunciation}]
+                    </span>
+                    <span id="shanbay_popover_stars" style="color:#ff5400!important;">
+                    </span>
+                    <span>
+                        <a id="shanbay_us_audio" href="#" class="speak us"><i class="icon icon-speak"></i></a>
+                    </span>
+
+                </div>
+                    <span>
+                        <button id="shanbay_add_word">添加</button>
+                    </span>
+                </div>
+            <div id="shanbay_definition">
+                ${data.definition.split('\n').join("<br/>")}
+            </div>
+            <div id="thesaurus">
+            </div>
+            <div id="macmillan">
+            </div>
+            <div id="verbal_adventages">
+            </div>
         <div>
         `
     )
 }
-var mousePos;
 function awesome_popver_position() {
     let w = window.innerWidth;
     let h = window.innerHeight;
@@ -35,21 +68,48 @@ function awesome_popver_position() {
     let y = mousePos.y;
 
     let r_diff = w - x;
-    let pop_w = $("#popover_html").getBoundingClientRect().width;
+    // let pop_w = $("#popover_html").getBoundingClientRect().width;
+    let pop_w = 300;
     // TODO: 计算弹出窗位置
     if (r_diff < pop_w) {
         x = x - pop_w;
     }
-    y = y + 10;
+    y = y + 20;
 
     return {x: x, y: y};
 }
+function hide_popver() {
+    $("#popover_html").css("display", "none");
+}
+function add_word(data) {
+    console.log('content.js add_word:', data);
+    chrome.runtime.sendMessage({method: "add_word", shanbay: data});
+}
 function show_shanbay(data) {
     console.log('content.js show_shanbay:', data);
+    last_shanbay_data = data;
+
     $("#popover_html").css("display", "block");
 
     let template = shanbay_template(data);
     $("#popover_html").html(template);
+    $('#shanbay_us_audio').click(function (e) {
+        e.preventDefault();
+        playAudio(last_shanbay_data.us_audio);
+        return false;
+    });
+    $('html').click(function (e) {
+        e.preventDefault();
+        hide_popver();
+        return false;
+    });
+    $('#shanbay_add_word').click(function (e) {
+        e.preventDefault();
+        let text = $('#shanbay_add_word').innerText;
+        if (text != '添加成功')
+            add_word(last_shanbay_data);
+        return false;
+    });
 
     let pos = awesome_popver_position();
     $("#popover_html").css("left", `${pos.x}px`);
@@ -64,6 +124,21 @@ function on_shanbay_data(request, sender, sendResponse) {
         show_shanbay(shanbay.data);
     }
 }
+function on_add_word_success(request, sender, sendResponse) {
+    console.log('content.js on_add_word_success:', request, sender);
+    let data = request.data;
+    console.log('content.js learning:', data);
+    $('#shanbay_add_word').innerText='添加成功';
+    $('#shanbay_add_word').remove();
+    console.log('#shanbay_add_word : ', ('#shanbay_add_word').innerText);
+}
+function on_thesaurus_data(request, sender, sendResponse) {
+    console.log('content.js on_thesaurus_data:', request, sender);
+// TODO: 显示不同词典的数据，抽象出通用的方法，可以显示不同的词典
+
+};
+
+
 function mouseMove(ev)
 {
     Ev= ev || window.event;
@@ -89,6 +164,12 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     switch (request.method) {
         case 'shanbay_data':
             on_shanbay_data(request, sender, sendResponse);
+            break;
+        case 'add_word_success':
+            on_add_word_success(request, sender, sendResponse);
+            break;
+        case 'thesaurus_data':
+            on_thesaurus_data(request, sender, sendResponse);
             break;
         default :
             sendResponse({data: [], error:'no method match'+ request.method, request:request}); // snub them.
--- js/iamyuhang.js
+++ js/iamyuhang.js
@@ -37,12 +37,71 @@ function on_selected_text(request, sender, sendResponse) {
     });
 
 }
+function playAudio(audio_url) {
+    if (audio_url) {
+        new Howl({
+            src: [audio_url],
+            volume: 1.0
+        }).play();
+    }
+};
+function on_add_word(request, sender, sendResponse) {
+    console.log('iamyuhang.js on_add_word:', request, sender);
+    if (iamyuhang_user_is_exist()) {
+        $.ajax({
+            url: 'https://iamyuhang.com/api/v1/words/learning/',
+            //url: 'http://localhost:3001/api/v1/words/learning/',
+            type: 'POST',
+            dataType: 'JSON',
+            contentType: "application/json; charset=utf-8",
+            data: JSON.stringify({
+                word: request.shanbay.content,
+                token: iamyuhang_user().authentication_token
+            }),
 
+            success: function (data) {
+                console.log('ajax success',data);
+                if (data.status == 'ok') {
+                    chrome.tabs.sendMessage(sender.tab.id, {
+                        method: 'add_word_success',
+                        data: data
+                    });
+                }
+            },
+            error: function (xhr,status, error) {
+                // chrome.tabs.sendMessage(tab.id, {
+                //     callback: 'addWord',
+                //     data: {msg: 'error', rsp: {}}
+                // });
+                console.log('error',xhr,status,error);
+            }
+        });
+    }
+    $.ajax({
+        url: url,
+        type: 'GET',
+        dataType: 'JSON',
+        contentType: "application/json; charset=utf-8",
+        success: function (data) {
+            console.log('shanbay api success', data);
+            chrome.tabs.sendMessage(sender.tab.id, {
+                method: 'shanbay_data',
+                shanbay: data
+            });
+        }
+    });
+};
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     console.log("iamyuhang.js received method: " + request);
     switch (request.method) {
         case 'selected_text':
             on_selected_text(request, sender, sendResponse);
+            break;
+        case 'add_word':
+            on_add_word(request, sender, sendResponse);
+            break;
+        case 'playAudio':
+            playAudio(request.data['audio_url']);
             break;
         case 'is_user_signed_on':
             isUserSignedOn();
